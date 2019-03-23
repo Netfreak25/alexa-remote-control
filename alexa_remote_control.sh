8e27a8cadd30d71065256280a51fdde03f7c6c58
Netfreak25 <moritzkuhn@MacBook-Pro-Work.fritz.box>
2019-03-23 18:32:11 +0100

    Update alexa_remote_control.sh

--- alexa_remote_control.sh
+++ alexa_remote_control.sh
@@ -617,7 +617,7 @@ TOTAL=`jq '.queueInfo | .[].title' $FILE.tmp | wc -l`
 
 
 echo "chapterCount:"${TOTAL}""
-jq '.queueInfo.media' $FILE.tmp
+jq '.queueInfo.media | .[0].' $FILE.tmp
 
 
 #${CURL} ${OPTS} -s -b ${COOKIE} -A "${BROWSER}" -H "DNT: 1" -H "Connection: keep-alive" -L\
