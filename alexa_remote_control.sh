713ca062fca731b5b49dedb2b715312f1e879c9f
Netfreak25 <moritzkuhn@MacBook-Pro-Work.fritz.box>
2019-03-23 21:54:46 +0100

    Update alexa_remote_control.sh

--- alexa_remote_control.sh
+++ alexa_remote_control.sh
@@ -687,7 +687,7 @@ ${CURL} ${OPTS} -s -b ${COOKIE} -A "${BROWSER}" -H "DNT: 1" -H "Connection: keep
 TOTAL=`jq '.audibleBookList | .[].title' /tmp/.alexa.audible.list.tmp | wc -l`
 
 echo "trackCount:"${TOTAL}""
-jq '.audibleBookList | .[] | .title,.asin' /tmp/.alexa.audible.list.tmp
+jq '.audibleBookList | .[] | .title,.asin' /tmp/.alexa.audible.list.tmp | tee /tmp/.alexa.audible.list_summary.tmp
 
 #rm -f ${FILE}.tmp
 }
