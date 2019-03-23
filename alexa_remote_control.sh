cf699d1e5f3aead6d1231d698f54dc6cd3fe1922
Netfreak25 <moritzkuhn@MacBook-Pro-Work.fritz.box>
2019-03-23 19:15:46 +0100

    Update alexa_remote_control.sh

--- alexa_remote_control.sh
+++ alexa_remote_control.sh
@@ -606,7 +606,16 @@ ${CURL} ${OPTS} -s -b ${COOKIE} -A "${BROWSER}" -H "DNT: 1" -H "Connection: keep
  -H "csrf: $(awk "\$0 ~/.${AMAZON}.*csrf[ \\s\\t]+/ {print \$7}" ${COOKIE})" -X POST -d "{\"deviceType\":\"${DEVICETYPE}\",\"deviceSerialNumber\":\"${DEVICESERIALNUMBER}\",\"mediaOwnerCustomerId\":\"${MEDIAOWNERCUSTOMERID}\",\"asin\":\"${ASIN}\"}" \
  "https://${ALEXA}/api/audible/queue-and-play?"
 
-sleep 5
+#pause
+## jump to champer 1
+${CURL} ${OPTS} -s -b ${COOKIE} -A "${BROWSER}" -H "DNT: 1" -H "Connection: keep-alive" -L\
+ -H "Content-Type: application/json; charset=UTF-8" -H "Referer: https://alexa.${AMAZON}/spa/index.html" -H "Origin: https://alexa.${AMAZON}"\
+ -H "csrf: $(awk "\$0 ~/.${AMAZON}.*csrf[ \\s\\t]+/ {print \$7}" ${COOKIE})" -X POST -d  "{\"type\":\"PauseCommand\",\"contentFocusClientId\":null}" \
+ "https://${ALEXA}/api/np/command?deviceSerialNumber=${DEVICESERIALNUMBER}&deviceType=${DEVICETYPE}&"
+
+}
+
+sleep 4
 # get chapter 1 mediaId
 TOTAL=0
 FILE=${TMP}/.alexa.audible.chapters.list
