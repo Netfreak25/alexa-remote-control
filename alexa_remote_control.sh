4bea93fc1e55fdf50136bbffdbab79aadda198c3
Netfreak25 <moritzkuhn@MacBook-Pro-Work.fritz.box>
2019-03-23 18:29:44 +0100

    Update alexa_remote_control.sh

--- alexa_remote_control.sh
+++ alexa_remote_control.sh
@@ -613,11 +613,11 @@ FILE=${TMP}/.alexa.audible.chapters.list
   -H "Content-Type: application/json; charset=UTF-8" -H "Referer: https://alexa.${AMAZON}/spa/index.html" -H "Origin: https://alexa.${AMAZON}"\
   -H "csrf: $(awk "\$0 ~/.${AMAZON}.*csrf[ \\s\\t]+/ {print \$7}" ${COOKIE})" -X GET \
   "https://${ALEXA}/api/np/queue?deviceSerialNumber=${DEVICESERIALNUMBER}&deviceType=${DEVICETYPE}" > ${FILE}.tmp
-TOTAL=`jq '.queueInfo | .[].title' $FILE | wc -l`
+TOTAL=`jq '.queueInfo | .[].title' $FILE.tmp | wc -l`
 
 
 echo "chapterCount:"${TOTAL}""
-jq '.queueInfo | .[].title' $FILE
+jq '.queueInfo | .[].title' $FILE.tmp
 
 
 #${CURL} ${OPTS} -s -b ${COOKIE} -A "${BROWSER}" -H "DNT: 1" -H "Connection: keep-alive" -L\
